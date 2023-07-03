4d352e87f88c63336dd47dfe8dcd027479103165
Viktor Belomestnov <belom88@yandex.ru>
2023-07-03 10:05:05 +0200

    fix(vehicle-layer): Scatterplot visualization

--- packages/vehicle-layer/src/lib/vehicle-layer.ts
+++ packages/vehicle-layer/src/lib/vehicle-layer.ts
@@ -6,13 +6,23 @@ import {
 
 type VehicleLayerProps<TProps> = ScenegraphLayerProps<TProps>;
 
-export class VehicleLayer<TProps> extends CompositeLayer<VehicleLayerProps<TProps>> {
+export class VehicleLayer<TProps> extends CompositeLayer<
+  VehicleLayerProps<TProps>
+> {
   override renderLayers(): Layer<object> | LayersList | null {
-    return new ScenegraphLayer({
-      ...this.props,
-      scenegraph: 'https://raw.githubusercontent.com/belom88/visgl/main/packages/vehicle-layer/models/transit-bus-low-poly-2.glb',
+    return [new ScenegraphLayer({
+      id: `${this.props.id}-scenegraph`,
+      data: this.props.data,
+      getPosition: this.props.getPosition,
+      getOrientation: this.props.getOrientation,
+      getColor: this.props.getColor,
+      scenegraph:
+        'https://raw.githubusercontent.com/belom88/visgl/main/packages/vehicle-layer/models/transit-bus-low-poly-2.glb',
       _lighting: 'pbr',
-    });
+      updateTriggers: {
+        ...this.props.updateTriggers,
+      },
+    })];
   }
 }
 
