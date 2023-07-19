f0561a640581df1a926996dee6ad9049d05285e4
Viktor Belomestnov <belom88@yandex.ru>
2023-07-19 02:23:03 +0200

    feat(testing-app): support Mapbox2 (#19)

--- packages/vehicle-layer-test-app/src/app/components/map-wrapper/map-wrapper.tsx
+++ packages/vehicle-layer-test-app/src/app/components/map-wrapper/map-wrapper.tsx
@@ -1,18 +1,26 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { Map as MaplibreMap } from 'react-map-gl/maplibre';
+import { Map as MapboxMap } from 'react-map-gl';
 import { useAppSelector } from '../../redux/hooks';
 import { selectVehiclesCount } from '../../redux/slices/layer-props.slice';
 import { selectAllRoutes } from '../../redux/slices/routes.slice';
 import { GeojsonRouteFeature } from '../../utils/load-routes';
-import DeckglWrapper from '../deckgl/deckgl-wrapper';
+import { createDeckglWith } from '../deckgl/deckgl-wrapper';
 import {
   AnimatedVehicle,
   Vehicle,
   animateVehicles,
   createVehicles,
 } from '../../utils/vehicles-utils';
-import { selectBaseMapMode } from '../../redux/slices/app.slice';
+import {
+  selectBaseMapMode,
+  selectMapProvider,
+} from '../../redux/slices/app.slice';
 import { BaseMapMode } from '../../types';
-import InterleavedMap from '../interleaved-map/interleaved-map';
+import { createInterleavedContainerWith } from '../interleaved-map/interleaved-map';
+import { BaseMapProviderId } from '../../constants/base-map-providers';
+
+const mapboxAccessToken = import.meta.env.VITE_MAPBOX_API_KEY;
 
 /* eslint-disable-next-line */
 export interface MapWrapperProps {}
@@ -29,8 +37,11 @@ export function MapWrapper(props: MapWrapperProps) {
   const [animatedVehicles, setAnimatedVehicles] = useState<AnimatedVehicle[]>(
     []
   );
+  const animatedVehiclesRef = useRef<AnimatedVehicle[]>([]);
+  animatedVehiclesRef.current = animatedVehicles;
 
   const baseMapMode = useAppSelector(selectBaseMapMode);
+  const mapProvider = useAppSelector(selectMapProvider);
 
   const animateLayer = useCallback(() => {
     animationStarted.current = true;
@@ -61,13 +72,42 @@ export function MapWrapper(props: MapWrapperProps) {
     vehiclesRef.current = createVehicles(vehiclesCount, routes);
   }, [vehiclesCount, routes]);
 
+  const DeckglComponent = useMemo(() => {
+    switch (mapProvider.id) {
+      case BaseMapProviderId.maplibre:
+        return createDeckglWith(MaplibreMap);
+      case BaseMapProviderId.mapbox2:
+        return createDeckglWith(
+          MapboxMap,
+          mapboxAccessToken,
+          'mapbox://styles/mapbox/streets-v12'
+        );
+      default:
+        return null;
+    }
+  }, [mapProvider]);
+
+  const InterleavedComponent = useMemo(() => {
+    switch (mapProvider.id) {
+      case BaseMapProviderId.maplibre:
+        return createInterleavedContainerWith(mapProvider.id);
+      case BaseMapProviderId.mapbox2:
+        return createInterleavedContainerWith(
+          mapProvider.id,
+          'mapbox://styles/mapbox/streets-v12'
+        );
+      default:
+        return null;
+    }
+  }, [mapProvider]);
+
   return (
     <>
-      {baseMapMode === BaseMapMode.OVERLAID && (
-        <DeckglWrapper vehicles={animatedVehicles} />
+      {baseMapMode === BaseMapMode.OVERLAID && DeckglComponent && (
+        <DeckglComponent vehicles={animatedVehicles} />
       )}
-      {baseMapMode === BaseMapMode.INTERLEAVED && (
-        <InterleavedMap vehicles={animatedVehicles} />
+      {baseMapMode === BaseMapMode.INTERLEAVED && InterleavedComponent && (
+        <InterleavedComponent vehicles={animatedVehicles} />
       )}
     </>
   );
--- packages/vehicle-layer-test-app/src/app/hooks/use-mapbox-hook/use-mapbox-hook.spec.tsx
+++ packages/vehicle-layer-test-app/src/app/hooks/use-mapbox-hook/use-mapbox-hook.spec.tsx
@@ -1,8 +1,9 @@
 import { render, renderHook } from '@testing-library/react';
 import * as React from 'react';
 
-import useMaplibreHook from './use-maplibre-hook';
+import useMaplibreHook from './use-mapbox-hook';
 import { createStoreWith, createWrapper } from '../../utils/test-utils';
+import { BaseMapProviderId } from '../../constants/base-map-providers';
 
 vi.mock('maplibre-gl', () => {
   const Map = vi.fn().mockReturnValue({
@@ -11,15 +12,18 @@ vi.mock('maplibre-gl', () => {
   return { Map };
 });
 
-describe('useMaplibreHook', () => {
+describe('useMapboxHook', () => {
   it('should render successfully', () => {
     const ref = React.createRef<HTMLDivElement>();
     render(<div ref={ref} />);
 
     const store = createStoreWith({});
-    const { result } = renderHook(() => useMaplibreHook(ref), {
-      wrapper: createWrapper(store),
-    });
+    const { result } = renderHook(
+      () => useMaplibreHook(ref, BaseMapProviderId.maplibre),
+      {
+        wrapper: createWrapper(store),
+      }
+    );
 
     expect(result.current).toStrictEqual({ current: null });
   });
