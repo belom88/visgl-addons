fbc1cdccf2ed4005b88f4ba860be689d9d432569
Viktor Belomestnov <belom88@yandex.ru>
2023-08-15 10:15:57 +0200

    feat(testing-app): show picking info (#57)

--- packages/vehicle-layer-test-app/src/app/components/common-styled.tsx
+++ packages/vehicle-layer-test-app/src/app/components/common-styled.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Paper, styled as muiStyled } from '@mui/material';
 import styled from 'styled-components';
 
 export const StyledMapContainer = styled.div`
@@ -10,11 +10,27 @@ export const StyledMapContainer = styled.div`
   top: 0;
 `;
 
-export const StyledTopPanelsContainer = styled(Box)`
+export const StyledTopPanelsContainer = muiStyled(Box)`
   top: 0;
   left: 0;
   width: 99%;
   visibility: hidden;
   position: absolute;
   z-index: 1;
+`;
+
+export const StyledBottomRightContainer = muiStyled(Box)`
+  bottom: 1.5em;
+  right: 0.5em;
+  width: 40rem;
+  position: absolute;
+  z-index: 1;
+`;
+
+export const StyledMainPaper = muiStyled(Paper)<{ bgColor?: string }>`
+  padding: ${({ theme }) => theme.spacing(1)};
+  textalign: 'center';
+  background-color: ${({ theme, bgColor }) =>
+    bgColor || theme.palette.background.paper};
+  color:  ${({ theme, color }) => color || theme.palette.text.primary};
 `;
--- packages/vehicle-layer-test-app/src/app/pages/home/home.tsx
+++ packages/vehicle-layer-test-app/src/app/pages/home/home.tsx
@@ -1,13 +1,14 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
-import { useAppDispatch } from '../../redux/hooks';
-import { appActions } from '../../redux/slices/app.slice';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { appActions, selectPikckingData } from '../../redux/slices/app.slice';
 import LayerPropsPanel from '../../components/layer-props-panel/layer-props-panel';
 import TestCasesPanel from '../../components/test-cases-panel/test-cases-panel';
 import { BASE_MAP_PROVIDERS } from '../../constants/base-map-providers';
 import MapWrapper from '../../components/map-wrapper/map-wrapper';
 import Fps from '../../components/fps/fps';
+import PickingInfo from '../../components/picking-info/picking-info';
 
 /* eslint-disable-next-line */
 export interface HomeProps {}
@@ -15,6 +16,8 @@ export interface HomeProps {}
 export function Home(props: HomeProps) {
   const { baseMapProviderId } = useParams();
   const dispatch = useAppDispatch();
+  const pickingData = useAppSelector(selectPikckingData);
+
   useEffect(() => {
     const baseMapProvider = BASE_MAP_PROVIDERS.find(
       ({ id }) => id === baseMapProviderId
@@ -28,7 +31,8 @@ export function Home(props: HomeProps) {
     <Box>
       <TestCasesPanel />
       <Fps />
-      <LayerPropsPanel />
+      {!pickingData && <LayerPropsPanel />}
+      {pickingData && <PickingInfo />}
       <MapWrapper />
     </Box>
   );
