42efe2aeb5f9a9b09c7e81ef894deac324ab1470
Viktor Belomestnov <belom88@yandex.ru>
2023-08-24 09:16:11 +0200

    fix(testing-app): vehicle colors (#74)

--- packages/vehicle-layer-test-app/src/app/components/layer-props-panel/layer-props-panel.tsx
+++ packages/vehicle-layer-test-app/src/app/components/layer-props-panel/layer-props-panel.tsx
@@ -20,7 +20,6 @@ import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import SceneProps from './scene-props/scene-props';
 import VehicleLayerProps from './vehicle-layer-props/vehicle-layer-props';
 import { PopoverId, UseCaseId } from '../../types';
-import * as d3 from 'd3';
 import { SizeMode } from '@belom88/vehicle-layer';
 import { StyledBottomRightContainer, StyledMainPaper } from '../common-styled';
 import { selectMapProvider } from '../../redux/slices/app.slice';
@@ -31,13 +30,6 @@ const StyledContentContainer = styled(Box)`
   overflow: auto;
   padding: 0 1em;
 `;
-
-const rgbToHex = (rgb?: [number, number, number]): string => {
-  if (!rgb) {
-    return '#FFF';
-  }
-  return d3.rgb(...rgb).formatHex();
-};
 
 const a11yProps = (index: number) => {
   return {
@@ -67,30 +59,14 @@ export function LayerPropsPanel(props: LayerPropsPanelProps) {
   const vehicleCommonColor = useAppSelector((state) =>
     selectVehicleColor(state, PopoverId.VEHICLE_LAYER_COMMON_COLOR)
   );
-  const commonHexColor = useMemo(
-    () => rgbToHex(vehicleCommonColor),
-    [vehicleCommonColor]
-  );
-
   const vehicle3dColor = useAppSelector((state) =>
     selectVehicleColor(state, PopoverId.VEHICLE_LAYER_3D_COLOR)
   );
-  const d3HexColor = useMemo(() => rgbToHex(vehicle3dColor), [vehicle3dColor]);
-
   const vehicle2dForegroundColor = useAppSelector((state) =>
     selectVehicleColor(state, PopoverId.VEHICLE_LAYER_2D_FOREGROUND)
   );
-  const d2ForegroundHexColor = useMemo(
-    () => rgbToHex(vehicle2dForegroundColor),
-    [vehicle2dForegroundColor]
-  );
-
   const vehicle2dBackgroundColor = useAppSelector((state) =>
     selectVehicleColor(state, PopoverId.VEHICLE_LAYER_2D_BACKGROUND)
-  );
-  const d2BackgroundHexColor = useMemo(
-    () => rgbToHex(vehicle2dBackgroundColor),
-    [vehicle2dBackgroundColor]
   );
 
   const disableTerrain = useMemo(
@@ -166,10 +142,10 @@ export function LayerPropsPanel(props: LayerPropsPanelProps) {
             size={size}
             vehicleScale={vehicleScale}
             dimensionMode={dimensionMode}
-            commonHexColor={commonHexColor}
-            d3HexColor={d3HexColor}
-            d2ForegroundHexColor={d2ForegroundHexColor}
-            d2BackgroundHexColor={d2BackgroundHexColor}
+            vehicleCommonColor={vehicleCommonColor}
+            vehicle3dColor={vehicle3dColor}
+            vehicle2dForegroundColor={vehicle2dForegroundColor}
+            vehicle2dBackgroundColor={vehicle2dBackgroundColor}
             onSizeModeChange={(result: SizeMode) =>
               dispatch(layerPropsActions.setSizeMode(result))
             }
